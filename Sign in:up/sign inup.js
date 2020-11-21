0a08d329ca0b707672a4ca08e8b027e24d3a2dab
Guo0606 <74224294+Guo0606@users.noreply.github.com>
2020-11-21 17:30:20 -0500

    Update sign inup.js

--- Sign in:up/sign inup.js
+++ Sign in:up/sign inup.js
@@ -2,7 +2,6 @@ window.onload=function(){
         createCode(4);
         /*Password strength*/
         function setCss(_this,cssOption){
-            //判断节点类型
             if (!_this || _this.nodeType ===3 || _this.nodeType === 8 ||!_this.style) {
                 return;
             }
@@ -17,12 +16,6 @@ window.onload=function(){
         }
         function passwordStrength(passwordStrength,announcement_Password){
             var self = this;
-/*
-数字1，字母2，其他字符为3
-p<6时>> too short
-长度>=6，强度小于10，强度弱
-长度>=6，强度>=10且<15，强度中
-长度>=6，强度>=15，强*/
             passwordStrength.onkeyup = function(){
                 var _color = ["red","yellow","orange","green"],
                         msgs = ["Too short","weak","Medium","Strong"],
@@ -30,31 +23,29 @@ p<6时>> too short
                         _v= trim(passwordStrength.value)
                 _vL= _v.length,
                         i=0;
-
                 var charStrength = function(char){
-                    //计算单个字符强度
-                    if(char>=48 && char <=57){//数字
+                    if(char>=48 && char <=57){
                         return 1;
                     }
-                    if(char>=97 && char<=122){//小写
+                    if(char>=97 && char<=122){
                         return 2;
                     }else{
-                        return 3; //特殊字符
+                        return 3; 
                     }
                 }
 
-                if(_vL<6){//计算模式
+                if(_vL<6){
                     announcement_Password.innerText = msgs[0];
                     setCss(announcement_Password,{
                         "color":_color[0]
                     })
                 }else{
                     for(;i<_vL;i++){
-                        //遍历字符
+                       
                         _strength+=charStrength(_v.toLocaleLowerCase().charCodeAt(i));
                     }
                     if(_strength<10){
-                        //强度小于10
+                        
                         announcement_Password.innerText = msgs[1];
                         setCss(announcement_Password,{
                             "color":_color[1]
@@ -86,38 +77,33 @@ p<6时>> too short
 
    
 
-    //生成验证码的方法
+    //create verification code 
     function createCode(length) {
         var code = "";
-        var codeLength = parseInt(length); //验证码的长度
-        var checkCode = document.getElementById("checkCode");
-        ////所有候选组成验证码的字符，当然也可以用中文的
+        var codeLength = parseInt(length); 
+        var checkCode = document.getElementById("checkCode");        
         var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
         'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); 
-        //循环组成验证码的字符串
         for (var i = 0; i < codeLength; i++)
         {
-            //获取随机验证码下标
+           
             var charNum = Math.floor(Math.random() * 62);
-            //组合成指定字符验证码
-            code += codeChars[charNum];
+      	    code += codeChars[charNum];
         }
         if (checkCode)
         {
-            //为验证码区域添加样式名
             checkCode.className = "code";
-            //将生成验证码赋值到显示区
             checkCode.innerHTML = code;
         }
     }
     
-    //检查验证码是否正确
+    
     function validateCode()
     {
-        //获取显示区生成的验证码
+       
         var checkCode = document.getElementById("checkCode").innerHTML;
-        //获取输入的验证码
+       
         var inputCode = document.getElementById("inputCode").value;
         if (inputCode.length <= 0)
         {
